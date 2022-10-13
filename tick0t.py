64f51471846772e2b6ce513f819e279e4b941cf0
Drew <drew1681@gmail.com>
2022-10-13 13:25:18 -0500

    renamed nickfury to tick0t

---
+++ tick0t.py
@@ -0,0 +1,58 @@
+import utils
+import interactions
+
+token = open('token.txt','r').readlines()[0].strip('\n')
+tick0t = interactions.Client(token=token)
+TicketHandler = utils.TicketDict(deletion_after_close=False)
+
+@tick0t.event
+async def on_ready():
+    print("The bot is now online.")
+
+@tick0t.command(name="create_ticket",description="create a ticket",
+        options = [
+        interactions.Option(
+            name="name",
+            description="the name for the ticket",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ],
+)
+async def create_ticket(ctx,name:str):
+    TicketHandler.add_ticket(utils.Ticket(name, ctx.author.name))
+    await ctx.send(f"Your ticket has been registered with the ticket number {TicketHandler._counter}.")
+
+@tick0t.command(name="close_ticket",description="Close a ticket")
+async def close_ticket(ctx, index:int):
+    TicketHandler.close_ticket(index)
+    await ctx.send("The ticket has been closed.")
+
+@tick0t.command(name="display_ticket",description="Display a chosen ticket.")
+async def display_ticket(ctx, index:int):
+    fOutput = TicketHandler.display_ticket(index)
+    await ctx.send(fOutput)
+
+@tick0t.command(name="query_tickets",description="List Active Tickets")
+async def query_tickets(ctx):
+    fOutput = TicketHandler.query_tickets()
+    await ctx.send(fOutput)
+
+@tick0t.command(name="save_tickets",description="Make a backup of tickets")
+async def save_tickets(ctx):
+    TicketHandler.save_tickets()
+    await ctx.send("Tickets are now backed up.")
+
+@tick0t.command(name="reload_tickets",description="Load Tickets from a backup")
+async def reload_tickets(ctx):
+    ret = TicketHandler.reload_tickets()
+    await ctx.send(f"Tickets are rolled back to last save at {ret}")
+
+@tick0t.command(
+        name="show_help",
+    description="Display a help message",
+)
+async def show_help(ctx: interactions.CommandContext):
+    await ctx.send("Hello world! ")
+
+tick0t.start()
